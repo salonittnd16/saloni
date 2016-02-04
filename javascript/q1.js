69d0495c4edd26cd05c0af729501da5a167de05d
Abhinav <abhinav.srivastava@intelligrape.com>
2016-02-04 22:39:57 +0530

    Added javascript excercise on GIT.

---
+++ javascript/q1.js
@@ -0,0 +1,5 @@
+var p=prompt("enter principal","principal");
+var r=prompt("enter rate","rate");
+var t=prompt("enter years ","time");
+var si=((p*r*t)/100);
+document.write("simple interest is:"+amt);
