d6144c6d254e2ced774b1d47ef3b8ee521b6aeaf
salonittnd16 <saloni.sharma@tothenew.com>
2016-02-05 16:10:34 +0530

    adding complete assignment

---
+++ q1.groovy
@@ -0,0 +1,49 @@
+class Person
+{
+  String name,gender,address;
+  Integer age;
+  
+  String  getName()
+  {
+   return name;
+  }
+  String  getGender()
+  {
+   return gender;
+  }
+  String  getAddress()
+  {
+  return address
+  }
+  Integer getAge()
+  {
+   return age;
+  }
+  
+  String setName()
+  {
+  this.name=name
+  name
+  }
+  
+  
+  String setGender()
+  {
+  this.gender=gender
+  return gender;
+  }
+  String setAddress()
+  {
+  this.address=address
+  address
+  }
+  Integer setAge()
+  {
+  this.age=age
+  age
+  }
+  
+  
+  }
+  
+  
