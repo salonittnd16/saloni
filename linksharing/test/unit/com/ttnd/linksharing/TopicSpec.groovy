73b20edadf98c8eb81338e21e399283966ad43c2
salonittnd16 <saloni.sharma@tothenew.com>
2016-02-12 11:41:16 +0530

    test cases for TopicSpec

--- linksharing/test/unit/com/ttnd/linksharing/TopicSpec.groovy
+++ linksharing/test/unit/com/ttnd/linksharing/TopicSpec.groovy
@@ -1,6 +1,8 @@
 package com.ttnd.linksharing
 
+import com.ttnd.linksharing.Enum.Visibility
 import grails.test.mixin.TestFor
+import org.omg.CORBA.PUBLIC_MEMBER
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -22,11 +24,11 @@ class TopicSpec extends Specification {
     void "test all topic validations"() {
 
         setup:
-        User user=new User();
+       // User user=new User();
         //User user = new User(firstName:"saloni", lastName: "sharma", email: "saloni@gmail.com", password: "hello", userName: "sa",
              //   admin: true, active: true);
-        user.metaClass.validate={ return true}
-        Topic topic = new Topic(name:name,createdBy:user);
+       // user.metaClass.validate={ return true}
+        Topic topic = new Topic(name:name,createdBy:author,visibility:visibility);
 
 
         when:
@@ -36,8 +38,11 @@ class TopicSpec extends Specification {
         result==valid
 
         where:
-        name | valid
-        "grails" | true
+        name     | author     | visibility         | valid
+        "grails" | new User() | Visibility.PUBLIC  | true
+        ""       | new User() | Visibility.PRIVATE | false
+        null     | new User() | Visibility.PUBLIC  | false
+        "mean"   | null       | Visibility.PRIVATE | false
 
     }
 
