060dc5200ad76471c040ab5b7e0bd2c7b1c79d14
salonittnd16 <saloni.sharma@tothenew.com>
2016-02-15 13:21:41 +0530

    q22.groovy corrected

--- q22.groovy
+++ q22.groovy
@@ -4,3 +4,15 @@ Map m = ['Computing' : ['Computing' : 600, 'Information Systems' : 300],'Enginee
 //a) there are 3 university departments
 //b)2 programs delivered
 //c)200 students enrolled in civil engineering program
+​Map m = ['Computing' : ['Computing' : 600, 'Information Systems' : 300],'Engineering' : ['Civil' : 200, 'Mechanical': 100],
+'Management' : ['Management' : 800] ]
+
+//a)
+println m.keySet().size()
+
+//b)
+println m.get('Computing').size()
+
+//c)
+println m.get('Engineering').get('Civil')
+​
