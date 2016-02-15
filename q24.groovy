5878454f71a58dbd662cc378d9971767e7a94f0f
salonittnd16 <saloni.sharma@tothenew.com>
2016-02-15 12:55:57 +0530

    q24.groovy corrected

--- q24.groovy
+++ q24.groovy
@@ -5,8 +5,14 @@ def returnValueOfKey()
 String s="http://www.google.com?name=johny&age=20&hobby=cricket" 
 List l=s.tokenize("?")
 println l
+List l3=[]
+Map m=[:]
 List l1= (l[1].tokenize("&"))
-return l1[0].tokenize("=")
+l1.each{ 
+        l3=it.tokenize("=")
+        println l3         
+        m.put(l3[0],l3[1])
+      }
 
 }
 println returnValueOfKey()
