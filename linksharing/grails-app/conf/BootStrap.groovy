920ae7aa6cc176d9705b226eb15496de025ab409
salonittnd16 <saloni.sharma@tothenew.com>
2016-02-20 22:12:12 +0530

    createReadingItems and createResourceRating

--- linksharing/grails-app/conf/BootStrap.groovy
+++ linksharing/grails-app/conf/BootStrap.groovy
@@ -7,7 +7,9 @@ import com.ttnd.linksharing.Resource
 import com.ttnd.linksharing.Subscription
 import com.ttnd.linksharing.Topic
 import com.ttnd.linksharing.User
-import com.ttnd.linksharing.Constants.Constant;
+import com.ttnd.linksharing.ResourceRating
+import com.ttnd.linksharing.Constants.Constant
+import org.xhtmlrenderer.css.parser.property.PrimitivePropertyBuilders;
 
 
 class BootStrap {
@@ -17,8 +19,9 @@ class BootStrap {
         List<User> users = createUsers()
         List<Topic> topics = createTopics(users)
         List<Resource> resources = createResources(topics)
-        List<Subscription> subscriptions = createSubscriptions(users,topics)
-        List<ReadingItem> readingItems=createReadingItems(resources)
+        List<Subscription> subscriptions = createSubscriptions(users, topics)
+        List<ReadingItem> readingItems = createReadingItems(users, topics)
+        List<ResourceRating> resourceRatings=createResourceRating(users,resources)
 
         //println(grailsApplication.config.grails.testvalue)
 
@@ -66,6 +69,7 @@ class BootStrap {
                     Topic topic = new Topic(name: "Topic${it}", createdBy: user, visibility: Visibility.PUBLIC);
                     topics.add(topic)
                     if (user.addToTopics(topic).save(flush: true))
+
                         log.info "---------${topic.name} added for $user--------\n"
                     else
                         log.error("error creating ${topic.name} for user $user")
@@ -79,48 +83,56 @@ class BootStrap {
     }
 
 
-    List<Resource> createResources(topics) {
+    List<Resource> createResources(List<Topic> topics) {
         List<Resource> resources = []
 
         if (DocumentResource.count == 0 && LinkResource.count == 0) {
 
-            topics.each {
+            topics.each { topic ->
 
-                DocumentResource documentResource1 = new DocumentResource(filePath: "a/b/c", description: "description${it.name}",
-                        createdBy: it.createdBy,
-                        topic: it)
-                DocumentResource documentResource2 = new DocumentResource(filePath: "a/b/c", description: "description${it.name}",
-                        createdBy: it.createdBy,
-                        topic: it)
+                DocumentResource documentResource1 = new DocumentResource(filePath: "a/b/c", description: "description${topic.name}",
+                        createdBy: topic.createdBy,
+                        topic: topic)
+                DocumentResource documentResource2 = new DocumentResource(filePath: "a/b/c", description: "description${topic.name}",
+                        createdBy: topic.createdBy,
+                        topic: topic)
 
-                LinkResource linkResource1 = new LinkResource(url: "http://", description: "description${it.name}",
-                        createdBy: it.createdBy, topic: it)
-                LinkResource linkResource2 = new LinkResource(url: "http://", description: "description${it.name}",
-                        createdBy: it.createdBy, topic: it)
+                LinkResource linkResource1 = new LinkResource(url: "http://www.blogjava.net/BlueSUN", description: "description${topic.name}",
+                        createdBy: topic.createdBy, topic: topic)
+                LinkResource linkResource2 = new LinkResource(url: "http://www.blogjava.net/BlueSUN", description: "description${topic.name}",
+                        createdBy: topic.createdBy, topic: topic)
 
                 if (documentResource1.save()) {
                     resources.add(documentResource1)
-                    log.info "---------${documentResource1.description} added for $it--------\n"
+                    topic.addToResources(documentResource1)
+                    topic.createdBy.addToResources(documentResource1)
+                    log.info "---------${documentResource1.description} added for $topic--------\n"
                 } else
-                    log.error("error creating ${documentResource1.description} for user $it")
+                    log.error("error creating ${documentResource1.description} for user $topic")
 
                 if (documentResource2.save()) {
                     resources.add(documentResource2)
-                    log.info "---------${documentResource2.description} added for $it--------\n"
+                    topic.addToResources(documentResource1)
+                    topic.createdBy.addToResources(documentResource1)
+                    log.info "---------${documentResource2.description} added for $topic--------\n"
                 } else
-                    log.error("error creating ${documentResource2.description} for user $it")
+                    log.error("error creating ${documentResource2.description} for user $topic")
 
                 if (linkResource1.save()) {
                     resources.add(linkResource1)
-                    log.info "---------${linkResource1.description} added for $it--------\n"
+                    topic.addToResources(linkResource1)
+                    topic.createdBy.addToResources(linkResource1)
+                    log.info "---------${linkResource1.description} added for $topic--------\n"
                 } else
-                    log.error("error creating ${linkResource1.description} for user $it")
+                    log.error("error creating ${linkResource1.description} for user $topic")
 
                 if (linkResource2.save()) {
                     resources.add(linkResource2)
-                    log.info "---------${linkResource2.description} added for $it--------\n"
+                    topic.addToResources(linkResource2)
+                    topic.createdBy.addToResources(linkResource2)
+                    log.info "---------${linkResource2.description} added for $topic--------\n"
                 } else
-                    log.error("error creating ${linkResource2.description} for user $it")
+                    log.error("error creating ${linkResource2.description} for user $topic")
 
 
             }
@@ -132,39 +144,99 @@ class BootStrap {
         }
         resources
 
-
     }
 
-    List<Subscription> createSubscriptions(List<User> users,List<Topic> topics) {
+    List<Subscription> createSubscriptions(List<User> users, List<Topic> topics) {
         List<Subscription> subscriptions = []
-            users.each { User user ->
-                topics.each {
-                    Topic topic ->
-                        if (topic.createdBy != user) {
+        users.each { User user ->
+            topics.each {
+                Topic topic ->
+                    if (topic.createdBy != user) {
 
-                            Subscription subscription = new Subscription(user: user, topic: topic, seriousness: Seriousness.VERY_SERIOUS)
-                            if (subscription.save(flush: true)) {
+                        Subscription subscription = new Subscription(user: user, topic: topic, seriousness: Seriousness.VERY_SERIOUS)
+                        if (subscription.save(flush: true)) {
 
-                                subscriptions.add(subscription)
-                                log.info("${user} subscribed to ${topic}")
-                            }
-                            else
-                                log.error("error creating ${subscription} for user $user")
-                        }
-                }
+                            subscriptions.add(subscription)
+                            log.info("${user} subscribed to ${topic}")
+                        } else
+                            log.error("error creating ${subscription} for user $user")
+                    }
             }
+        }
 
 
         subscriptions
 
     }
 
-    List<ReadingItem> createReadingItems(List<Resource> resources)
-    {
+    List<ReadingItem> createReadingItems(List<User> users, List<Topic> topics) {
+
+        List<ReadingItem> readingItems = []
+
+        users.each
+                {
+                    user ->
+                        topics.each
+                                {
+                                    topic ->
+                                        if (Subscription.findByUserAndTopic(user, topic)) {
+
+                                            topic.resources.each
+                                                    {
+                                                        resource ->
+                                                            if (resource.createdBy != user && !user.readingItems?.contains(resource)) {
+                                                                ReadingItem readingItem = new ReadingItem(user: user, resource: resource, isRead: false)
 
 
+
+                                                                if (readingItem.save()) {
+                                                                    readingItems.add(readingItem)
+                                                                    user.addToReadingItems(readingItem)
+                                                                    log.info "${readingItem} saved successfully"
+                                                                } else
+                                                                    log.error "unable to save reading item: ${readingItem.errors.allErrors}"
+                                                            }
+
+                                                    }
+
+                                        }
+
+
+
+                                }
+
+                }
+
+        readingItems
+    }
+
+
+
+    List<ResourceRating> createResourceRating(List<User> users,List<ReadingItem> readingItemsnew) {
+        List<ResourceRating> resourceRatings = []
+        users.each { user ->
+            readingItemsnew = ReadingItem.findAllByUser(user)
+            readingItemsnew.each { readingItems1 ->
+                if (!readingItems1.isRead) {
+                    ResourceRating resourceRating = new ResourceRating(user: readingItems1.user,resource: readingItems1.resource,
+                            score: 4)
+
+                    if (resourceRating.save()) {
+
+                        resourceRatings.add(resourceRating)
+                        user.addToResourceRatings(resourceRating)
+                        log.info "${resourceRating} saved successfully"
+                    } else
+                        log.error "Error saving ${resourceRating.errors.allErrors}"
+                }
+            }
+
+        }
+
+        resourceRatings
 
     }
+
 
 
     def destroy = {
